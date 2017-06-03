e45b03954e9eb81dd83c3a83ef80cbdefc5022f5
wujixialan <1571449371@qq.com>
2017-06-03 20:50:47 +0800

    第5次

---
+++ src/com/zxg/mybatis/Department.java
@@ -0,0 +1,33 @@
+package com.zxg.mybatis;
+
+/**
+ * Created by zxg on 2017/6/3.
+ */
+public class Department {
+    private Integer id;
+    private String deptName;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDeptName() {
+        return deptName;
+    }
+
+    public void setDeptName(String deptName) {
+        this.deptName = deptName;
+    }
+
+    @Override
+    public String toString() {
+        return "Department{" +
+                "id=" + id +
+                ", deptName='" + deptName + '\'' +
+                '}';
+    }
+}
--- src/com/zxg/mybatis/Employee.java
+++ src/com/zxg/mybatis/Employee.java
@@ -11,6 +11,15 @@ public class Employee {
     private String lastName;
     private String email;
     private String gender;
+    private Department dept;
+
+    public Department getDept() {
+        return dept;
+    }
+
+    public void setDept(Department dept) {
+        this.dept = dept;
+    }
 
     public Integer getId() {
         return id;
@@ -51,6 +60,7 @@ public class Employee {
                 ", lastName='" + lastName + '\'' +
                 ", email='" + email + '\'' +
                 ", gender='" + gender + '\'' +
+                ", dept=" + dept +
                 '}';
     }
 }
